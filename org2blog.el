2e93a402f3d644262f976c28d779ac1359e61054
Jacek Zlydach <temporal.pl@gmail.com>
2011-02-23 23:46:22 +0100

    Fixed exporting code in <code> tag for my blog, which uses this tag for source code.

--- org2blog.el
+++ org2blog.el
@@ -391,7 +391,7 @@ Entry to this mode calls the value of `org2blog/wp-mode-hook'."
         (setq start-pos (point-min))
         (goto-char start-pos)
         (while (re-search-forward 
-                "\\(<\\(pre\\|blockquote\\).*?>\\(.\\|[[:space:]]\\)*?</\\2.*?>\\)"
+                "\\(<\\(pre\\|blockquote\\|code\\).*?>\\(.\\|[[:space:]]\\)*?</\\2.*?>\\)"
                 nil t 1)
           (setq end-pos (match-beginning 0))
           (replace-regexp "\\\n" " " nil start-pos end-pos)
